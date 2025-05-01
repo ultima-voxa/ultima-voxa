7eb5acde84becdb448a7471fdf200a1006517170
ultima-voxa <thomasbrian78@orange.fr>
2025-05-01 22:57:31 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import UltimaVoxaLanding from './UltimaVoxaLanding';
+import Submit from './pages/Submit';
+import './index.css';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import UltimaVoxaLanding from "./UltimaVoxaLanding";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <UltimaVoxaLanding />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UltimaVoxaLanding />} />
+        <Route path="/submit" element={<Submit />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
